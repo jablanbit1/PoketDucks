bbe75f07a9564a017bad40afd4ea987129fc0d6a
Nemanja Jablanovic <nikolajokic516@gmail.com>
2023-09-18 12:15:52 +0200

    Fali da se importuje natašin deo

--- Teren/Teren.py
+++ Teren/Teren.py
@@ -1,1 +1,23 @@
 import pygame as pg
+from ../main import *
+
+
+class Pravougaonik:
+    def __init__(self, length, height):
+        self.length = length
+        self.height = height
+    
+    def crtanje(self):
+        pg.draw.rectangle(0, 0, self.length, self.height)
+
+
+
+
+def main():
+    Teren = Pravougaonik(SCREEN_WIDTH, SCREEN_HEIGHT)
+    Teren.crtanje()
+
+
+
+if __name__ == "__main__":
+    main()
