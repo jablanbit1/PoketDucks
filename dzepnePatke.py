eab18ce41edc2222c997a88d35a2f3bc8b62cca3
Jovan Belevski <jokikosmajac@gmail.com>
2023-09-18 12:27:06 +0200

    Definicija klase patkica

--- dzepnePatke.py
+++ dzepnePatke.py
@@ -1,2 +1,23 @@
 import pygame as pg
 
+class Patkica:
+
+    def __init__(self, x, y, r1):
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.hp = 100
+        self.r1 = r1
+        #self.r2 = r2
+
+
+    def crtaj(self, pozadina):
+        pg.draw.circle(pozadina, (0, 255, 255), (self.x, self.y),self.r1 )
+
+    def plivaj(self):
+        self.x += self.vx
+        
+    def puckaj():
+        pass
+      
