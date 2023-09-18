90f2b79c0d8d37bfb13b0a626fd695dfb97e90c5
Nemanja Jablanovic <nikolajokic516@gmail.com>
2023-09-18 13:55:50 +0200

    "Probajte da popravite"

--- Teren/Teren.py
+++ Teren/Teren.py
@@ -1,5 +1,5 @@
-import pygame as pg
-from ../main import *
+import pygame
+from PoketDucks import main
 
 
 class Pravougaonik:
@@ -8,7 +8,7 @@ class Pravougaonik:
         self.height = height
     
     def crtanje(self):
-        pg.draw.rectangle(0, 0, self.length, self.height)
+        pygame.draw.rectangle(0, 0, self.length, self.height)
 
 
 
