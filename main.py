0d9aa185c8c736639401760dc55eeac019d83928
Natasa Kovacevic <kovacevic.natasa@gmail.com>
2023-09-18 22:13:02 +0200

    JEJ RADI ZAMENA POTEZA I OBE LINIJE

--- main.py
+++ main.py
@@ -15,21 +15,15 @@ running = True
 patkica1 = Patkica(SCREEN_WIDTH / 6, SCREEN_HEIGHT - 132, 30)
 patkica2 = Patkica(SCREEN_WIDTH * 5 / 6, SCREEN_HEIGHT - 132, 30)
 
-<<<<<<< HEAD
-potez1 = True
-potez2 = False
-inklik = False
-kockicaa = False
-
-
-=======
 potez2 = False
 potez1 = True
 inklik = False
 kockicaa = False
-x1=0
-y1=0
->>>>>>> 10ee7ff6e54ddcaac9f1c0e20ec6ad0a37b84fa7
+x1 = 0
+y1 = 0
+
+sledeci = 2
+
 while running:
     screen.fill((200, 200, 240))
     
@@ -42,13 +36,12 @@ while running:
             poz1 = pg.mouse.get_pos()
             x1 = poz1[0]
             y1 = poz1[1]
-            
             inklik = True
         if inklik:
             if potez1:
-                    pg.draw.line(screen, (0, 0, 0),(patkica1.x, patkica1.y), (patkica1.x + (x1-tren[0]), patkica1.y + (y1-tren[1])))
+                    pg.draw.line(screen, (20, 20, 20), (patkica1.x, patkica1.y), (patkica1.x + (x1 - tren[0]), patkica1.y + (y1 - tren[1])), 6)
             if potez2:
-                    pg.draw.line(screen, (0, 0, 0),(patkica2.x, patkica2.y), (patkica2.x + (x1-tren[0]), patkica2.x + (y1-tren[1])))
+                    pg.draw.line(screen, (20, 20, 20), (patkica2.x, patkica2.y), (patkica2.x + (x1 - tren[0]), patkica2.y + (y1 - tren[1])), 6)
         if event.type == pg.MOUSEBUTTONUP and inklik:
             poz2 = pg.mouse.get_pos()
             x2 = poz2[0]
@@ -59,21 +52,34 @@ while running:
             if potez1:
                 k = Kockica(patkica1.x, patkica1.y, vx, vy)
                 k.crtaj(screen)
-
                 kockicaa = True
+                potez1 = False
+                sledeci = 2
                 
             if potez2:
                 k = Kockica(patkica2.x, patkica2.y, vx, vy)
                 k.crtaj(screen)
                 kockicaa = True
+                potez2 = False
+                sledeci = 1
                 
     
     patkica1.crtaj(screen)
     patkica2.crtaj(screen)
+    
     if kockicaa:
         k.crtaj(screen)
         k.mrdaj()
         k.gravitacija()
+        if k.x < 0 or k.x > SCREEN_WIDTH or k.y > SCREEN_HEIGHT:
+            if sledeci == 1:
+                potez1 = True
+                pass
+            if sledeci == 2:
+                potez2 = True
+                pass
+        
+    
                     
     Teren = Pravougaonik(SCREEN_WIDTH, 100)
     Teren.crtanje(screen, SCREEN_HEIGHT-100)
