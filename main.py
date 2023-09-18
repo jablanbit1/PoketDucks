51811a1a13fab904e39cf26e560610f07d487a05
Natasa Kovacevic <kovacevic.natasa@gmail.com>
2023-09-18 11:08:37 +0200

    Pygame prozor testiranje

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import pygame
 
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 480
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill((200, 200, 240))
+    pygame.display.flip()
+    clock.tick(60)
